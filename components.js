aa91083b2546a46b667850cb6dc0006047dd0ae2
Luke <none@none.com>
2014-02-23 17:43:22 +1100

    move to using angular-google-maps. use geolocation for map

--- components.js
+++ components.js
@@ -24,7 +24,7 @@ angular.module('components', [])
         '<div class="tabbable">' +
           '<ul class="nav nav-tabs">' +
             '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
-              '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
+              '<a href="" class="tab-header" ng-click="select(pane)">{{pane.title}}</a>' +
             '</li>' +
           '</ul>' +
           '<div class="tab-content" ng-transclude></div>' +
--- tides.js
+++ tides.js
@@ -1,77 +1,52 @@
 
-angular.module('app', ['components'])
- 
-.controller('about', function($scope, $locale) {
-  $scope.beers = [0, 1, 2, 3, 4, 5, 6];
-  if ($locale.id == 'en-us') {
-    $scope.beerForms = {
-      0: 'no beers',
-      one: '{} beer',
-      other: '{} beers'
-    };
-  } else {
-    $scope.beerForms = {
-      0: 'žiadne pivo',
-      one: '{} pivo',
-      few: '{} pivá',
-      other: '{} pív'
-    };
-  }
-});
-  
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-  
-function aboutController($scope) {
-	$scope.message = "Thanks for using my site, I hope you found it useful. ";
-}
-function TodoCtrl($scope) {
-  /* var mapElement = document.getElementById("map_canvas");
-  var myOptions = {
-    center: new google.maps.LatLng(-43.53567218583064,172.60057833726108),
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+
+function initializeMap() {
+  var centreLatLng = new google.maps.LatLng(-25.274398,133.775136);
+  var defaultMapViewOptions = {
+    zoom: 3,
+    centre: centreLatLng
   };
- 
-  map = new google.maps.Map(mapElement, myOptions);
-  var myLatlng = new google.maps.LatLng(-43.53567218583064,172.60057833726108);
-  var marker = new google.maps.Marker({
-	position: myLatlng,
+  var map = new google.maps.Map(document.getElementById('map-canvas'), defaultMapViewOptions);
+  var marker = null;
+  function updateMarker() {
+    if (marker) {
+      marker.setMap(null);
+    }
+    marker = new google.maps.Marker({
+	position: centreLatLng,
 	map: map,
-	title:''
-  });*/
-  
-
-  $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
- 
-  $scope.addTodo = function() {
-    $scope.todos.push({text:$scope.todoText, done:false});
-    $scope.todoText = '';
-  };
- 
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
+	draggable:true,
+	title:"Find tide times at this location"
     });
-    return count;
-  };
- 
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
+    
+  }
+  updateMarker();
+  //HTML5 geolocation - zoom to user's location
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      centreLatLng = new google.maps.LatLng(position.coords.latitude,  position.coords.longitude);
+      map.setZoom(5)
+      map.setCenter(centreLatLng);
+      updateMarker();
+    }, function(){});
+  }
+  console.log(marker.getPosition().lat())
 }
 
+//google.maps.event.addDomListener(window, 'load', initializeMap);
+  
+
+function tidesController($scope) {
+  $scope.map = {
+    center: {
+        latitude: 45,
+        longitude: -73
+    },
+    zoom: 8
+  };
+
+  //$scope.latlon = '';
+  
+}
+angular.module('app', ['components', 'google-maps']);
